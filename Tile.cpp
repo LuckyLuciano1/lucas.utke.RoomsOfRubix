06242035f59a7c49b8cb1adf2a508f20f79ba211
LuckyLuciano1 <lucasutkework@gmail.com>
2017-05-01 14:50:10 -0600

    Add files via upload

--- Tile.cpp
+++ Tile.cpp
@@ -12,11 +12,14 @@ void Tile::Init(ALLEGRO_BITMAP *TileImage, double x, double y, double z, int ima
 
 void Tile::Render()
 {
-	for (int a = 0; a < z / boundY; a++) {
-		al_draw_tinted_scaled_rotated_bitmap_region(image, 0, 200, imageboundX, imageboundY, transparency, boundX / 2, boundY / 2, x, y + z + (a*boundY), boundX / imageboundX, boundY / imageboundY, angle, FlipHorizontal);
-		al_draw_tinted_scaled_rotated_bitmap_region(image, 200, 200, imageboundX, imageboundY, al_map_rgba_f(0, 0, 0, .5), boundX / 2, boundY / 2, x, y + z + (a*boundY), boundX / imageboundX, boundY / imageboundY, angle, FlipHorizontal);
-	}
+	//for (int a = 0; a < (z / boundY); a++) {
+		al_draw_tinted_scaled_rotated_bitmap_region(image, 0, 200, imageboundX, imageboundY, transparency, boundX / 2, boundY / 2, x, y + z + 50, boundX / imageboundX, boundY / imageboundY, angle, FlipHorizontal);
+		//al_draw_tinted_scaled_rotated_bitmap_region(image, 200, 200, imageboundX, imageboundY, al_map_rgba_f(0, 0, 0, .5), boundX / 2, boundY / 2, x, y + z + (a*boundY), boundX / imageboundX, boundY / imageboundY, angle, FlipHorizontal);
+	//}
 	Object::Render();
+	
+	//al_draw_textf(font18, al_map_rgb(255, 255, 255), 5, 5, 0, "ROOM: %c", CurrentRoom);
+	
 }
 
 void Tile::Update(double cameraX, double cameraY)
